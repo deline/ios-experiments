ef68204b9b1739700bd0aeb50b0aa2e5083cffab
Deline Neo <delineneo@gmail.com>
2013-07-16 21:00:02 +1000

    Rework storyboard

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// US2FormValidator
+#define COCOAPODS_POD_AVAILABLE_US2FormValidator
+#define COCOAPODS_VERSION_MAJOR_US2FormValidator 1
+#define COCOAPODS_VERSION_MINOR_US2FormValidator 1
+#define COCOAPODS_VERSION_PATCH_US2FormValidator 1
+
